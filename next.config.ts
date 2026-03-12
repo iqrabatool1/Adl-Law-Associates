295daee4853cb12e6186d59a0c8bced3edd09219
Ali Haider <alihaiderturiofficial@gmail.com>
2026-03-12 10:25:26 +0500

    Refactor Next.js configuration to streamline output settings and update Turbopack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* We UNCOMMENTED 'export' so Next.js generates static HTML files 
-     that can be uploaded to your public_html folder.
-  */
   output: 'export', 
-  
-  // This helps with 404 issues on some servers
-  trailingSlash: true, 
-  
-  // Optional: only if you are using Turbopack
-  experimental: {
-    turbo: {
-       root: ".",
-    },
+  trailingSlash: true,
+  turbopack: {
+    root: ".",
   },
 };
 
